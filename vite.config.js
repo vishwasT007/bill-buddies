3613f787c8340c5f80a2adc63c9316d6c2c6c070
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2025-04-20 19:51:58 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist", // This tells Vite to use the dist folder for build output
+  },
   plugins: [react()],
   base: "/bill-buddies/", // Make sure this matches your repository name
 });
