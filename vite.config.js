d2b88c1779093dcbabfdafeea439bf8e5003752e
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2025-04-20 19:37:09 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./bill-buddies/",
+  base: "/bill-buddies/", // Make sure this matches your repository name
 });
