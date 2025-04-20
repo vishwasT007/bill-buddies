68687c55e80e67b155d85a3e9a203b543e0a685f
Vishwas Tarende <vishwastarende@Vishwass-MacBook-Pro.local>
2025-04-20 19:17:53 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/bill-buddies/", // This ensures the correct path prefix for assets
+  base: "/bill-buddies/",
 });
